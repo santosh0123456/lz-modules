3e4ed90915ab0588d5bb3c4f3ce9f62bf5e584fa
santosh0123456 <santosh.kumar@crestsolution.com>
2026-03-27 20:21:30 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,9 +6,20 @@ terraform {
     }
   }
 }
+
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
+variable "subscription_id" {}
+
 provider "azurerm" {
   features {}
   use_cli = false
+
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
 }
 resource "azurerm_resource_group" "network" {
   name = "rg_network_poc"
