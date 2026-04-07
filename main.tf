8db5996f90dca371de91251326e9d36bb12f5b34
santosh0123456 <santosh.kumar@crestsolution.com>
2026-04-07 15:42:45 +0800

    Update -4 main.tf

--- main.tf
+++ main.tf
@@ -32,10 +32,10 @@ provider "vault" {
     }
   }
 }
-#data "vault_azure_access_credentials" "creds" {
-#  backend = "azure"
-#  role    = "tfe-role"
-#  validate_creds              = false
+data "vault_azure_access_credentials" "creds" {
+  backend = "azure"
+  role    = "tfe-role"
+  validate_creds              = false
 #  num_sequential_successes    = 1
 #  num_seconds_between_tests   = 1
 #  max_cred_validation_seconds = 300
