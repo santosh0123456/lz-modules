d7b855ecdd4c883a8c024f939cacaaa19a79858f
santosh0123456 <santosh.kumar@crestsolution.com>
2026-04-08 11:54:18 +0800

    Update-now-16 main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     vault   = { source = "hashicorp/vault",    version = "~> 3.0" }
     azurerm = { source = "hashicorp/azurerm",  version = "~> 3.100" }
-    #time    = { source = "hashicorp/time",     version = "~> 0.9" }
+    time    = { source = "hashicorp/time",     version = "~> 0.9" }
   }
 }
 
