8e465c5c7f975ed14b7414593ea507e0a0d8cdee
santosh0123456 <santosh.kumar@crestsolution.com>
2026-05-02 01:25:59 +0800

    Update NSG Map Subnet main.tf

--- main.tf
+++ main.tf
@@ -154,7 +154,7 @@ resource "azurerm_network_interface" "mariadb" {
 # ----------------------------
 # Attach NSG to NIC
 # ----------------------------
-resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
+resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
   subnet_id                 = azurerm_subnet.subnet.id
   #network_interface_id      = azurerm_network_interface.nic.id
   network_security_group_id = azurerm_network_security_group.nsg.id
