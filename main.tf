6be098a264fc738f5ee702619c63adfa479cfd62
santosh0123456 <santosh.kumar@crestsolution.com>
2026-05-04 16:59:09 +0800

    Update remove oidc token main.tf

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ provider "azurerm" {
   client_id       = var.client_id       # "19baf6b7-69ab-443e-ad52-77ee501d2ac0"
   tenant_id       = var.tenant_id       # "c267b313-f395-45c7-82f9-325e4d530d90"
   subscription_id = var.subscription_id # "71dc99cb-2548-4b6b-bf46-cd57e81fccaa"
-  oidc_token_file = var.oidc_token_file
+  # oidc_token_file = var.oidc_token_file
 #
 }
 
