51028a13c960e4050cf9d98d217e16993d3c8f6f
santosh0123456 <santosh.kumar@crestsolution.com>
2026-05-04 16:37:44 +0800

    Update vars main.tf

--- main.tf
+++ main.tf
@@ -21,6 +21,8 @@ variable "github_token" {}
 variable "client_id" {}
 variable "tenant_id" {}
 variable "subscription_id" {}
+variable "oidc_token_file" {}
+variable "use_oidc" {}
 
 provider "azurerm" {
   features {}
@@ -30,6 +32,7 @@ provider "azurerm" {
   client_id       = var.client_id       # "19baf6b7-69ab-443e-ad52-77ee501d2ac0"
   tenant_id       = var.tenant_id       # "c267b313-f395-45c7-82f9-325e4d530d90"
   subscription_id = var.subscription_id # "71dc99cb-2548-4b6b-bf46-cd57e81fccaa"
+  oidc_token_file = var.oidc_token_file
 #
 }
 
