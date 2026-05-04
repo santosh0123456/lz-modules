3ff20bc678cc365770f940e9c37cecd8c3aa35b7
santosh0123456 <santosh.kumar@crestsolution.com>
2026-05-04 16:09:29 +0800

    Update add arm vars main.tf

--- main.tf
+++ main.tf
@@ -12,21 +12,24 @@ terraform {
     storage_account_name = "crestsolution"
     container_name       = "tfestorage"
     key                  = "rhel9/rhel9.tfstate"
-    #access_key           = "QxfH2CS+RT/SF6wAjAHYIprdWXtuIM1BErWrs74V3gNp9mxfvv64/tXumr6WvQNXLM3XkgWK8IKRlJyYHdfFLg=="
-    
+       
     use_oidc = true
     use_cli = false
   }
 }
 variable "github_token" {}
+variable "client_id" {}
+variable "tenant_id" {}
+variable "subscription_id" {}
 
 provider "azurerm" {
   features {}
   use_oidc = true
   use_cli  = false         # ← REQUIRED
-  #client_id       = "19baf6b7-69ab-443e-ad52-77ee501d2ac0"
-  #tenant_id       = "c267b313-f395-45c7-82f9-325e4d530d90"
-  #subscription_id = "71dc99cb-2548-4b6b-bf46-cd57e81fccaa"
+
+  client_id       = var.client_id       # "19baf6b7-69ab-443e-ad52-77ee501d2ac0"
+  tenant_id       = var.tenant_id       # "c267b313-f395-45c7-82f9-325e4d530d90"
+  subscription_id = var.subscription_id # "71dc99cb-2548-4b6b-bf46-cd57e81fccaa"
 #
 }
 
