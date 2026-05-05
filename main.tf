99462a6a1c25f2cc4baeaef49b64b7e7893126e8
santosh0123456 <santosh.kumar@crestsolution.com>
2026-05-05 20:32:31 +0800

    Update passing gittoken main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.7"   # must be 3.7+ for OIDC
     }
-  }
+  } 
   backend "azurerm" {
     resource_group_name  = "crestsolution"
     storage_account_name = "crestsolution"
