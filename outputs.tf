faee4d84b82c865da8969e1c45ef6579b3bb515b
santosh0123456 <santosh.kumar@crestsolution.com>
2026-05-29 23:40:51 +0800

    Fix output key from 'tomcat' to 'keycloak'

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "vm_ips" {
   value = {
     nginx  = azurerm_public_ip.nginx.ip_address
-    tomcat = azurerm_public_ip.keycloak.ip_address
+    keycloak = azurerm_public_ip.keycloak.ip_address
     db     = azurerm_public_ip.mariadb.ip_address
   }
 }
