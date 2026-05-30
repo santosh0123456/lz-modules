cfa66675b8e007bf09d14958ab36037bb452b059
santosh0123456 <santosh.kumar@crestsolution.com>
2026-05-30 21:39:11 +0800

    Modify security rules for HTTPS and MySQL access
    Updated security rules to allow HTTPS traffic and modified MySQL rule direction.

--- main.tf
+++ main.tf
@@ -151,9 +151,20 @@ resource "azurerm_network_security_group" "nsg" {
   }
 
   security_rule {
-    name                       = "allow-mysql"
+    name                       = "allow-https"
     priority                   = 1002
-    direction                  = "Outbound"
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "allow-mysql"
+    priority                   = 1003
+    direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
